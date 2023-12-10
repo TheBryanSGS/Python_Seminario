32f5d2c73ff57863a4a907f718cb386a8617100d
erik777ramos <estefany.r.g7777@gmail.com>
2023-12-10 03:26:23 -0500

    Interfaz

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path #definir rutas o urls
-from .views import index, login, opciones # desde views trae la funcion index
+from . import views
+from .views import index, login, opciones, registro, reporte# desde views trae la funcion index
 
 urlpatterns =[
-    path('', index),
-    path('login',login),
-    path('opciones',opciones)
+    path('',views.index,name ="index"),
+    path('login/', views.login, name ='login'),
+    path('opciones',views.opciones, name = 'opciones'),
+    path('registro', views.registro, name ="registro"),
+    path('reporte', views.reporte, name ="reporte")
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
 def login(request):
     return render(request, 'login.html')
 def opciones(request):
     return render(request, 'opciones.html')
+def registro (request):
+    return render(request, 'registro.html')
+def reporte (request):
+    return render(request, 'reporte.html')
